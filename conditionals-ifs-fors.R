40b0220ac4cfc5b6f9070212fafba0b4a79ceafc
NatBonnet <nathaliehbonnet@gmail.com>
2025-08-13 12:32:56 -0700

    for loop practice

--- conditionals-ifs-fors.R
+++ conditionals-ifs-fors.R
@@ -54,5 +54,53 @@ switch(species,
 
 # see next week dplyr::case_when()
 
+# writing for loops
+
+dog_names <- c("Teddy", "Khora", "Banjo", "Waffle")
+print("my dog's name is Teddy")
+
+paste("my dog's name is", dog_names[1])
+
+print(paste("my dog's name is", dog_names[1])) # this would be super annoying to copy and paste over and over again
+
+pupster <- 1
+print(paste("my dog's name is", dog_names[pupster]))
+
+i <- dog_names
+for(i in dog_names){
+  print(paste("my dog's name is", i))
+}
+
+# another for loop example
+
+mass <- seq(from = 0, to = 3, by= 0.5)
+i <- mass[1]
+
+for(i in mass){
+  new_value <- i+2
+  print(new_value)
+}
+
+# practice same example but with indexing
+
+for(i in 1:length(mass) #defining iterator using the length of the vector (defining values for i) this returns a list of 7 in this case because you are indexing from 0 to 3 in increments of 0.5 so 7 positions in the vector- same as writing seq_along(mass)
+    ){
+new_value <- mass[i] +2
+print(new_value)
+}
 
 
+# antoher example with iterating by position(indexing)
+tree_height <-c(1,2,6,10)
+
+# example for the first case
+tree_height[1]+ tree_height[2]
+
+# convert into a generalizable expression
+i <- 1
+tree_height[i] + tree_height[i+1]
+
+for(i in seq_along(tree_height)){
+  val <- tree_height[i] + tree_height[i+1]
+  print(val)
+}
